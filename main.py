d014c806ed77594e0d4dd12353ea6c09881f379c
pidan <pidan@123.com>
2025-02-16 17:22:58 +0800

    board bounce

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ pygame.display.set_caption("bounce_game")
 
 class Ball():
     def __init__(self):
-        self.x = 20
-        self.y = 20
+        self.x = 200
+        self.y = 200
 
         self.radius = 10
         self.color = (255, 255, 255)
@@ -28,18 +28,63 @@ class Ball():
         speed = self.speed
         self.x += self.diriction[0]*speed
         self.y += self.diriction[1]*speed
+
         if self.x + self.radius >= screen.get_width() or self.x-self.radius <= 0:
             self.diriction[0] *= -1
         if self.y + self.radius >= screen.get_height():
-            self.diriction[1] *= -1
-        if self.y - self.radius <= 0:
             fail()
+        if self.y - self.radius <= 0:
+            self.diriction[1] *= -1
+
+        if self.x + self.radius >= board.get_x_y_length()[0] and self.x - self.radius <= board.get_x_y_length()[0] + board.get_x_y_length()[2]:
+            if self.y + self.radius >= board.get_x_y_length()[1] and self.y - self.radius <= board.get_x_y_length()[1] + 10:
+                self.diriction[1] *= -1
+
+class Board():
+    def __init__(self):
+        self.x = 400
+        self.y = 500
+        self.length = 100
+
+        self.color = (255,100,100)
+
+    def get_x_y_length(self):
+        return self.x, self.y, self.length
+
+    def input(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.x -= 10
+            if event.key == pygame.K_RIGHT:
+                self.x += 10
+
+            if self.x+self.length >= 800:
+                self.x = 800 - self.length
+            if self.x <= 0:
+                self.x = 0
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.length, 10))
+
+class Brick():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, 10, 10))
+
+def make_brick():
+    for i in range()
+
+is_fail = False
 
 def fail():
-    global running
-    running = False
+    global is_fail
+    is_fail = True
 
 ball = Ball()
+board = Board()
 
 while running:
     time.sleep(1/FPS)
@@ -48,10 +93,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    ball.move()
+    if not is_fail:
+        ball.move()
+        board.input(event)
 
-    screen.fill((0, 0, 0), (0, 0, 800, 600))
-    ball.draw(screen)
+        screen.fill((0, 0, 0), (0, 0, 800, 600))
+        ball.draw(screen)
+        board.draw(screen)
+        
+    else:
+        font = pygame.font.SysFont(None, 50)
+        text = font.render("You Fail!", True, (255, 255, 255))
+        screen.blit(text, (400, 250))
+
     pygame.display.flip()
 
 pygame.quit()
