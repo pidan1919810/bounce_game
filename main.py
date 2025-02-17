f63fa78cd1ac972533569ee8479058498c1bc72c
pidan <pidan@123.com>
2025-02-17 23:27:38 +0800

    adding brick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from setting import *
 import time
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -11,40 +12,64 @@ pygame.display.set_caption("bounce_game")
 
 class Ball():
     def __init__(self):
-        self.x = 200
-        self.y = 200
+        self.x = 400
+        self.y = 250 
 
         self.radius = 10
         self.color = (255, 255, 255)
         
-        self.diriction = [1, 1] # [x, y]
+        self.diriction = [random.randrange(-1,2,2), 1] # [x, y]
         self.speed = 5 
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
 
     # move the ball ,rebounce on wall, change diriction
-    def move(self):
+    def move(self,brick,board):
+        rad = random.random()*10%2-1
+        print(rad)
+
         speed = self.speed
         self.x += self.diriction[0]*speed
         self.y += self.diriction[1]*speed
 
         if self.x + self.radius >= screen.get_width() or self.x-self.radius <= 0:
             self.diriction[0] *= -1
+            self.diriction[1] += rad
         if self.y + self.radius >= screen.get_height():
             fail()
-        if self.y - self.radius <= 0:
+        if self.y  <= 0:
             self.diriction[1] *= -1
+            self.diriction[1] += rad
 
         if self.x + self.radius >= board.get_x_y_length()[0] and self.x - self.radius <= board.get_x_y_length()[0] + board.get_x_y_length()[2]:
             if self.y + self.radius >= board.get_x_y_length()[1] and self.y - self.radius <= board.get_x_y_length()[1] + 10:
                 self.diriction[1] *= -1
+                self.diriction[1] += rad
+
+        for b in brick:
+            global brick_list
+            if self.x + self.radius >= b.get_x_y_size()[0] and self.x - self.radius <= b.get_x_y_size()[0] + b.get_x_y_size()[2]:
+                if self.y + self.radius >= b.get_x_y_size()[1] and self.y - self.radius <= b.get_x_y_size()[1] + b.get_x_y_size()[2]:
+                    self.diriction[1] *= -1 
+                    self.diriction[1] += rad
+                    brick_list.remove(b)
+                    break
+
+            if self.y + self.radius >= b.get_x_y_size()[1] and self.y - self.radius <= b.get_x_y_size()[1] + b.get_x_y_size()[2]:
+                if self.x + self.radius >= b.get_x_y_size()[0] and self.x - self.radius <= b.get_x_y_size()[0] + b.get_x_y_size()[2]:
+                    self.diriction[0] *= -1 
+                    self.diriction[1] += rad
+                    if b in brick_list:
+                        brick_list.remove(b)
+                        break
+            
 
 class Board():
     def __init__(self):
         self.x = 400
         self.y = 500
-        self.length = 100
+        self.length = 400
 
         self.color = (255,100,100)
 
@@ -66,16 +91,27 @@ class Board():
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.length, 10))
 
+brick_size = 20
+
 class Brick():
     def __init__(self,x,y):
         self.x = x
         self.y = y
+        self.size = brick_size
     
     def draw(self, screen):
-        pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, 10, 10))
+        pygame.draw.rect(screen, (200, 200, 200), (self.x+1, self.y+1, self.size-1, self.size-1))
+        pygame.draw.lines(screen, (255, 255, 255), True, [(self.x, self.y), (self.x+self.size, self.y), (self.x+self.size, self.y+self.size), (self.x, self.y+self.size)], 1)
+
+    def get_x_y_size(self):
+        return self.x, self.y, self.size
+    
+brick_list = []
 
 def make_brick():
-    for i in range()
+    for x in range(0, 800, 20):
+        for y in range(0, 200, 20):
+            brick_list.append(Brick(x, y))
 
 is_fail = False
 
@@ -86,19 +122,39 @@ def fail():
 ball = Ball()
 board = Board()
 
-while running:
-    time.sleep(1/FPS)
+make_brick()
 
+coutner = 0
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    time.sleep(1/FPS)
+
+    coutner += 1
+    if coutner <= 3*FPS:
+        sec = coutner//FPS
+
+        screen.fill((0, 0, 0), (0, 0, 800, 600))
+        font = pygame.font.SysFont(None, 50)
+        text = font.render(f"game will be start in {3-sec} seconds", True, (255, 255, 255))
+        screen.blit(text, (200, 250))
+        pygame.display.flip()
+        continue
+
+    
+
     if not is_fail:
-        ball.move()
+        ball.move(brick_list,board)
         board.input(event)
 
         screen.fill((0, 0, 0), (0, 0, 800, 600))
         ball.draw(screen)
+
+        for b in brick_list:
+            b.draw(screen)
+
         board.draw(screen)
         
     else:
